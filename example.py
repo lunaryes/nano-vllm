9c9958ded3ac6d0887fad4cd9bf2270784316271
lunaryes <2047932939@qq.com>
2026-09-24 22:16:23 +0800

    test1

--- example.py
+++ example.py
@@ -13,6 +13,12 @@ def main():
         "introduce yourself",
         "list all prime numbers within 100",
     ]
+    
+    print(f"testForNOtemplate:{prompts[0]!r}")
+    ids=tokenizer(prompts[0])
+    print(ids[:],"len:",len(ids))
+    print("eos_id:",tokenizer.eos_token_id)
+
     prompts = [
         tokenizer.apply_chat_template(
             [{"role": "user", "content": prompt}],
